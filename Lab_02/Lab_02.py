009ec160886c77e06e0730be24ccbf847363b8f9
adrynvelasquez <adrynvelasquez@tamu.edu>
2024-02-04 10:26:40 -0600

    Commit lab_02 exercise

--- Lab_02/Lab_02.py
+++ Lab_02/Lab_02.py
@@ -1,32 +1,28 @@
-#Part 1 - Multiply all list items together.
-#Choose a function that will multiply each in the list to the preceeding number.
-def mul_list(LIST):
-    result1 = 1
-    for i in LIST:
-        result1 = result1 * i
-    return result1
-    
+#Part 1###Multiply together all items in a list.
 part1 = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
-print(mul_list(part1))
-print("The answer to question 1 is: ", mul_list(part1))
-#In this section I had to figure out an alternative definition to the add to the loop shown in the tutorial.
-#I found an example of a multiplication definition to use on a list 'mul_list(LIST)' 
+result1 = 1
 
-###Part 2 - Add all list items together.
+for i in part1:
+    result1 = result1 * i
+print(result1)
+print("The answer to question 1 is: ", result1)
+
+#Part 2##Add items in list together.
 part2 = [-1, 23, 483, 8573, -13847, -381569, 1652337, 718522177]
 result2 = 0
 
 for i in part2:
-    result2 = 0 + i
+    result2 = result2 + i
 
-print("The answer to question 2 is: ", result2)
+print("The answer to part 2 is: ", result2)
 
-###Part 3 - In the given list, only add together items which are even. 
-# Use Modelo operation to determine if a number is even or odd.
+#Part 3###Add together all items in list that are even integers. Use Modulo operation.
 part3 = [146, 875, 911, 83, 81, 439, 44, 5, 46, 76, 61, 68, 1, 14, 38, 26, 21] 
 result3 = 0
 
 for i in part3:
-    result3 = 0 + i
+    if i % 2 == 0:
+        result3 = result3 + i
 
-print("The answer to question 3 is: " result3)
+print("The answer to question 3 is: ", result3)
+
